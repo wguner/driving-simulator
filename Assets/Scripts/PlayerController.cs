c10ac13cd8321dceeadee09baa4484424df3529b
wguner <setenayguner@gmail.com>
2024-01-22 15:42:35 -0800

    added horizontal and vertical movement, keybinds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
         
     }
 }
